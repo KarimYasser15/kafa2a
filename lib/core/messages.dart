a93db38a04419e1b76632a2ae6ab1de25b49508a
Karim Yasser <karimyasser1510@gmail.com>
2025-04-24 21:20:14 +0200

    Added Navigation to login for service provider and fixed typo in messages

--- lib/core/messages.dart
+++ lib/core/messages.dart
@@ -1,7 +1,7 @@
 class Messages {
   static const String emailRequired = "Please Enter Your Email";
-  static const String invalidEmail = "Please Enter a valid Email";
-  static const String passwordRequired = "Please enter a password";
+  static const String invalidEmail = "Please Enter a Valid Email";
+  static const String passwordRequired = "Please Enter Your Password";
   static const String passwordIsSmall =
       "Password must be at least 6 characters";
 }
--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -35,7 +35,8 @@ class OnboardingScreen extends StatelessWidget {
             ),
             DefaultElevatedButton(
               label: StringsManager.serviceProvider,
-              onPressed: () {},
+              onPressed: () => Navigator.pushNamed(context, RoutesManager.login,
+                  arguments: "provider"),
             )
           ],
         ),
